3cb8eb021bf65108c0ac8a15d33c592c9446da18
Mark <73297926+markrachapoom@users.noreply.github.com>
2022-08-10 19:18:52 +0900

    Add CTA to judge page

--- src/pages/judge.tsx
+++ src/pages/judge.tsx
@@ -1,8 +1,8 @@
 import { NextPage } from "next";
-import React, { FC } from "react";
+import React, { FC, useState } from "react";
 
 // CHakra UI
-import { Box, Flex, Image, Text, VStack } from "@chakra-ui/react";
+import { Box, Button, Flex, Image, setScript, Text, VStack } from "@chakra-ui/react";
 import { AppContainer } from "src/components/appContainer";
 
 const Judge: NextPage = () => {
@@ -44,20 +44,99 @@ const Judge: NextPage = () => {
         "https://images.unsplash.com/photo-1569982175971-d92b01cf8694?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1335&q=80",
     },
   ];
+
+
+  const [currentTab, setCurrentTab] = useState("completed")
+
   return (
     <AppContainer>
-      <Flex direction={"column"} gap={"2em"} maxWidth={"960px"} width={"100%"}>
-        {companies.map((company) => {
-          return (
-            <CompanyStatus
-              key={company.name}
-              name={company.name}
-              status={company.status}
-              imageURI={company.imageURI}
-            />
-          );
-        })}
-      </Flex>
+      <VStack
+        maxWidth={"960px"}
+        width={"100%"}
+        gap={"2em"}
+      >
+
+        {/* BUTTONS ROW */}
+
+        <Flex
+          fontSize="5xl"
+          fontWeight="bold"
+          lineHeight="shorter"
+          justifyContent={"flex-start"}
+          width={"100%"}
+        >
+          Company List
+        </Flex>
+
+        <Flex
+          justifyContent={"flex-start"}
+          width={"100%"}
+          gap={"1em"}
+        >
+
+          {/* WAITING FOR JUDGE */}
+          <Button
+            backgroundColor={currentTab === 'waiting for judge' ? "black" : "transparent"}
+            color={currentTab === 'waiting for judge' ? "white" : "black"}
+            borderRadius={999}
+            borderColor={"background.lightGray"}
+            borderWidth={"3px"}
+            onClick={() => {
+              setCurrentTab("waiting for judge")
+            }}
+          >
+            Waiting for judge
+          </Button>
+
+          {/* PROCESSING */}
+          <Button
+            backgroundColor={currentTab === 'processing' ? "black" : "transparent"}
+            color={currentTab === 'processing' ? "white" : "black"}
+            borderRadius={999}
+            borderColor={"background.lightGray"}
+            borderWidth={"3px"}
+            onClick={() => {
+              setCurrentTab("processing")
+            }}
+          >
+            Processing
+          </Button>
+
+          {/* COMPLETED */}
+          <Button
+            backgroundColor={currentTab === 'completed' ? "black" : "transparent"}
+            color={currentTab === 'completed' ? "white" : "black"}
+            borderRadius={999}
+            borderColor={"background.lightGray"}
+            borderWidth={"3px"}
+            onClick={() => {
+              setCurrentTab("completed")
+            }}
+          >
+            Completed
+          </Button>
+
+        </Flex>
+
+        <Flex
+          width={"100%"}
+          direction={"column"}
+          gap={"2em"}
+        >
+          { companies.filter((company) => {
+            return company.status == currentTab
+          }).map((company) => {
+            return (
+              <CompanyStatus
+                key={company.name}
+                name={company.name}
+                status={company.status}
+                imageURI={company.imageURI}
+              />
+            );
+          })}
+        </Flex>
+      </VStack>
     </AppContainer>
   );
 };
